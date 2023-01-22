79094ae8f61224107670cc6a37d9baeef08e1b3b
Daniel Yadeta <107301873+Danielyadeta@users.noreply.github.com>
2023-01-22 09:43:28 -0800

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,22 +1,11 @@
 #!/usr/bin/env bash
-#Bash script that sets up your web servers for the deployment of web_static
-
-sudo apt-get update -y
-sudo apt-get install nginx -y
-
-#create folders
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-
-#HTML file
-echo "Holberton By Dlhz" > /data/web_static/releases/test/index.html
-
-#Symbolic link
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
-
-#Set ownership
-sudo chown -R ubuntu:ubuntu /data/
-
-#Nginx config
-sudo sed -i "38i \\\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n" /etc/nginx/sites-available/default
-sudo service nginx restart
+# Write a Bash script that sets up your web servers for the deployment of web_static
+apt-get -y update
+apt-get -y install nginx
+ufw allow 'Nginx HTTP'
+mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "I'm at MAGFest 2020" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test /data/web_static/current
+chown -R ubuntu:ubuntu /data
+sed -i '/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
+service nginx restart
